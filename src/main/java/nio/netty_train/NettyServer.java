3b5b8456f686b52c6c6a8aa50ed3d9c945a1bc14
nydia_lhq <nydia_lhq@hotmail.com>
2021-05-26 00:22:49 +0800

    'nio'

--- src/main/java/nio/netty_train/NettyServer.java
+++ src/main/java/nio/netty_train/NettyServer.java
@@ -1,5 +1,13 @@
 package nio.netty_train;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * @Auther: hqlv
  * @Date: 2021/5/25 23:14
@@ -8,8 +16,25 @@ package nio.netty_train;
 public class NettyServer {
 
 
-    public static void main(String[] args) {
+    public void bind(int port) {
 
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                //初始化服务端可连接队列
+                .option(ChannelOption.SO_BACKLOG, 100)
+                .childHandler(new ChildC)
+
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel>{
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new ());
+        }
     }
 
 }
