1bbeaaf17a8441ebec92d1e2aaef5783f0971053
nydia_lhq <nydia_lhq@hotmail.com>
2021-06-20 18:09:32 +0800

    使用shardingsphere动态切换数据源并且读写分离

---
+++ mysqlMasterSlaveV2/src/main/java/com/nydia/modules/entity/User.java
@@ -0,0 +1,22 @@
+package com.nydia.modules.entity;
+
+import lombok.*;
+
+/**
+ * @Auther: hqlv
+ * @Date: 2021/6/20 01:19
+ * @Description:
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Integer userId;
+    private String userName;
+    private String password;
+    private String nickName;
+    private String idCard;
+
+}
