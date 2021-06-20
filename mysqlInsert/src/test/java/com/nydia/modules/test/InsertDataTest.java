8dbf9af66c1133d2c61d2b64a13a0f183985ac1c
nydia_lhq <nydia_lhq@hotmail.com>
2021-06-20 23:47:53 +0800

    使用shardingsphere动态切换数据源并且读写分离

--- mysqlInsert/src/test/java/com/nydia/modules/test/InsertDataTest.java
+++ mysqlInsert/src/test/java/com/nydia/modules/test/InsertDataTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -27,7 +28,7 @@ public class InsertDataTest {
 
     @Test
     public void insertData() {
-        insertDataByDruidIn100Thread();
+        insertByJdbc();
     }
 
     //1. 普通的单线程，利用框架加上Druid连接池
@@ -68,6 +69,50 @@ public class InsertDataTest {
         }
         executorService.shutdown();
         System.out.println("执行时间1000000次插入的时间：" + (new Date().getTime() - runTime.get()) + "（毫秒）");
+    }
+
+    //3. Jdbc插入
+    public void insertByJdbc(){
+        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        String DB_URL = "jdbc:mysql://localhost:3316/db";
+        final String USER = "root";
+        final String PASS = "";
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // 注册 JDBC 驱动
+            Class.forName(JDBC_DRIVER);
+            // 打开链接
+            System.out.println("连接数据库...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "insert into `geek_user` ( `user_name`, `password`, `nick_name`, `id_card`) values('王五','123456','小五','2344556666')";
+            stmt.executeUpdate(sql);
+            // 完成后关闭
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
     }
 
 
