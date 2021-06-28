be958e297f08d1110960ddbec3f6a3beb4d8b0e1
lvhuaqiang <lvhuaqiang@iboxpay.com>
2021-06-28 09:51:11 +0800

    第八周作业

---
+++ docs/create_data.sql
@@ -0,0 +1,23 @@
+
+DROP PROCEDURE IF EXISTS `add_data`;
+DELIMITER ;;
+CREATE PROCEDURE `add_data`()
+BEGIN
+    DECLARE i INT;
+    DECLARE j INT;
+    SET i=0;
+    WHILE i<=100 DO
+        SET j=1;
+        WHILE j<100 DO
+            INSERT INTO db(username) VALUES('²âÊÔ');
+            SET j = j + 1;
+        END WHILE;
+        SET i = i + 1;
+    END WHILE;
+END
+;;
+
+
+-- CALL add_data();
+
+-- TRUNCATE TABLE db;
