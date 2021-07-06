f5b15d58d4b2b084fdda9d5c382950e6de41b151
lvhuaqiang <lvhuaqiang@iboxpay.com>
2021-07-06 19:16:15 +0800

    周9作业

---
+++ rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/aop/ServiceHelper.java
@@ -0,0 +1,14 @@
+package io.kimmking.rpcfx.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * AOP
+ */
+@Component
+@Aspect  //标识为一个切面供容器读取
+public class ServiceHelper {
+
+
+}
