2005a74daa5ade91c4e2cc700f0bba5fa8841a50
nydia_lhq <nydia_lhq@hotmail.com>
2021-07-07 22:36:15 +0800

    springaop

--- rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/aop/ServiceHelper.java
+++ rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/aop/ServiceHelper.java
@@ -1,6 +1,9 @@
 package io.kimmking.rpcfx.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,6 +12,15 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect  //标识为一个切面供容器读取
 public class ServiceHelper {
+
+    @Pointcut("execution(* io.kimmking.rpcfx.demo.api.*.*(..))")
+    public void p(){}
+
+    @Before("p()")
+    public void beforeLog(ProceedingJoinPoint joinpoint) {
+        System.out.println("开始执行前置通知  日志记录:"+id);
+    }
+
 
 
 }
