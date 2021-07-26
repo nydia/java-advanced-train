86fa91ae901ea0fbb1060bcceb4aa72e4669b433
nydia_lhq <nydia_lhq@hotmail.com>
2021-07-26 22:17:48 +0800

    第十二周作业

--- 08cache/hazelcast/src/main/java/io/kimmking/cache/hazelcast/HazelcastDemo.java
+++ 08cache/hazelcast/src/main/java/io/kimmking/cache/hazelcast/HazelcastDemo.java
@@ -1,9 +1,18 @@
 package io.kimmking.cache.hazelcast;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.util.Map;
+
 public class HazelcastDemo {
 
     public static void main(String[] args) {
-
+        //嵌入式方式
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        Map<String,String> customers = hazelcastInstance.getMap("orders");
+        customers.put("order", "money order");
+        System.out.println(customers.get("order"));
     }
 
 }
