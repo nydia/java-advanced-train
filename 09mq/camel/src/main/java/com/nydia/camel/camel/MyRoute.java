9e042ce7ac62863e0ac26123acfbd133f86c2b5a
nydia_lhq <nydia_lhq@hotmail.com>
2021-08-10 00:21:07 +0800

    add file

--- 09mq/camel/src/main/java/com/nydia/camel/camel/MyRoute.java
+++ 09mq/camel/src/main/java/com/nydia/camel/camel/MyRoute.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyRoute extends RouteBuilder {
 
+    //activemq -> camel -> rabbitmq
     @Override
     public void configure() throws Exception {
-        from("...").to("...");
+//        from("...").to("...");
+        from("tcp://127.0.0.1:61616:test.queue").to("rabbitmq://192.168.99.100:5672/QUEUE_A?username=guest&password=guest&exchangeType=topic&autoDelete=false&queue=QUEUE_A");
     }
 
 }
