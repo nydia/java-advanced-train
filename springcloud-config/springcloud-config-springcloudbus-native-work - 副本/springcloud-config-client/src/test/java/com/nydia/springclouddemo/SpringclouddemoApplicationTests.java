ecd33d8890119a97f9ee285bb52d54f646783c91
nydia <nydia_lvhq@sina.cn>
2022-06-21 18:19:18 +0800

    add

---
+++ springcloud-config/springcloud-config-springcloudbus-native-work - 副本/springcloud-config-client/src/test/java/com/nydia/springclouddemo/SpringclouddemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nydia.springclouddemo;
+
+///import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringclouddemoApplicationTests {
+
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
