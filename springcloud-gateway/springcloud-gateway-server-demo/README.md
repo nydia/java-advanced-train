8f1fd52ebac1e32738a7bfcd6dac9901810f876d
nydia <nydia_lvhq@sina.cn>
2022-06-27 14:53:08 +0800

    ad

---
+++ springcloud-gateway/springcloud-gateway-server-demo/README.md
@@ -0,0 +1,10 @@
+# H2 
+
+- SpringCloud http://localhost:8082/h2-console
+
+- H2 http://localhost:8080/h2-conso*le
+
+# Spring Cloud Gateway
+
+- curl http://localhost:8082/actuator/gateway/routes
+- curl http://localhost:8082/test
---
+++ springcloud-gateway/springcloud-gateway-server-demo/src/main/java/com/nydia/springclouddemo/feign/PathTwoController.java
@@ -0,0 +1,27 @@
+package com.nydia.springclouddemo.feign;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.webflux.ProxyExchange;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+/**
+ * @Description TODO
+ * @Date 2022/5/15 13:46
+ * @Created by <a href="mailto:nydia_lhq@hotmail.com">lvhuaqiang</a>
+ */
+@RestController
+@RequestMapping
+public class PathTwoController {
+
+    @GetMapping("/route2/test")
+    public String getPng() throws Exception {
+        return "success-path2";
+    }
+
+}
