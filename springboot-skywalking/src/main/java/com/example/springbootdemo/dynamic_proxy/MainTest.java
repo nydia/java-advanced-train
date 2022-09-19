2647fdab9017828128486c4d1b530ebc338b0d6a
‘nydia’ <nydia_lhq@hotmail.com>
2022-09-19 23:16:47 +0800

    ad

---
+++ springboot-skywalking/src/main/java/com/example/springbootdemo/dynamic_proxy/MainTest.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemo.dynamic_proxy;
+
+public class MainTest {
+ 
+	public static void main(String[] args) throws Exception {
+		BaseDaoProxyFactory<UserTestDao> factory = new BaseDaoProxyFactory<UserTestDao>();
+		factory.setInterfaceClass(UserTestDao.class);
+		UserTestDao testDao = factory.getObject();
+		testDao.delete(1l);
+		testDao.get(3l);
+	}
+}
