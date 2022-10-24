d2b5d509d2c6a1015f91770a0b9545bde9cf9089
‘nydia’ <nydia_lhq@hotmail.com>
2022-10-24 23:16:26 +0800

    addd

--- springboot-mpush/src/test/java/com/example/springbootdemo/SpringbootDemoApplicationTests.java
+++ springboot-mpush/src/test/java/com/example/springbootdemo/SpringbootDemoApplicationTests.java
@@ -1,5 +1,11 @@
 package com.example.springbootdemo;
 
+import com.mpush.api.spi.common.MQClient;
+import com.mpush.api.spi.common.MQClientFactory;
+import com.mpush.api.spi.common.MQMessageReceiver;
+import com.mpush.bootstrap.ServerLauncher;
+import com.mpush.core.MPushServer;
+import com.mpush.core.push.PushCenter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +16,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringbootDemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testPush() {
+		ServerLauncher serverLauncher = new ServerLauncher();
+		MPushServer mPushServer = serverLauncher.getMPushServer();
+		PushCenter pushCenter = mPushServer.getPushCenter();
+
+		//IPushMessage message = null;
+		pushCenter.push(null);
+	}
+
+	@Test
+	public void testPublish(){
+		MQClient mqClient = MQClientFactory.create();
+		//MQMessageReceiver mqMessageReceiver = new Biz
+		mqClient.subscribe("bizMessage", null);
 	}
 
 }
