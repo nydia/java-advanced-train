11c4639ad83f28678e2a89d4b2281e54f05592ae
‘nydia’ <nydia_lhq@hotmail.com>
2022-12-04 01:08:16 +0800

    add

---
+++ algorithm/src/main/java/com/nydia/algorithm/P2128.java
@@ -0,0 +1,34 @@
+package com.nydia.algorithm;
+
+/**
+ * @Auther: nydia_lhq@hotmail.com
+ * @Date: 2022/11/30 23:00
+ * @Description: P28
+ */
+public class P2128 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode deleteNode(ListNode head, int val) {
+
+        while (true){
+            ListNode pre = head;
+            ListNode next = head;
+            if(head.val != val){
+                
+            }
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
