65d001621b601762f8d3c9fb90b92058482efc83
‘nydia’ <nydia_lhq@hotmail.com>
2022-12-07 23:16:21 +0800

    p2131

---
+++ algorithm/src/main/go/P2131.go
@@ -0,0 +1,25 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	if head.Val == val {
+		return head.Next
+	}
+	pre := head
+	cur := head.Next
+	for cur != nil && cur.Val != val {
+		pre = cur
+		cur = cur.Next
+	}
+	if cur != nil {
+		pre.Next = cur.Next
+	}
+	return head
+}
---
+++ algorithm/src/main/java/com/nydia/algorithm/P2131.java
@@ -0,0 +1,34 @@
+package com.nydia.algorithm;
+
+/**
+ * @Auther: nydia_lhq@hotmail.com
+ * @Date: 2022/12/7 22:44
+ * @Description: P2131
+ */
+public class P2131 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode deleteNode(ListNode head, int val) {
+        if (head.val == val) return head.next;
+        ListNode pre = head, cur = head.next;
+        while (cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if (cur != null) pre.next = cur.next;
+        return head;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
