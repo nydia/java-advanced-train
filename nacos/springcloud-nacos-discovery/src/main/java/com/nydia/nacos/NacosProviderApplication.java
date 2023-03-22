a751d1fea58c182af6531e64910efa2565069c60
nydia <nydia_lvhq@sina.cn>
2023-03-22 19:46:30 +0800

    nacos

---
+++ nacos/springcloud-nacos-discovery/src/main/java/com/nydia/nacos/NacosProviderApplication.java
@@ -0,0 +1,29 @@
+package com.nydia.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderApplication {
+
+	public static void main(String[] args) {
+		//修改nacos的目录(必须在下面的启动命令上面执行）
+		System.setProperty("JM.SNAPSHOT.PATH","C:\\temp\\nacos-client");
+
+		SpringApplication.run(NacosProviderApplication.class, args);
+	}
+
+	@RestController
+	class EchoController {
+		@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+		public String echo(@PathVariable String string) {
+			return "Hello Nacos Discovery " + string;
+		}
+	}
+}
