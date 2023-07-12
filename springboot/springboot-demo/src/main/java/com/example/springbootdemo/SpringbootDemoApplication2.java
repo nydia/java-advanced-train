d313db40353ae86323571a693051591573831ec4
‘nydia’ <nydia_lhq@hotmail.com>
2023-07-13 00:33:02 +0800

    mybatis自动装配的demo

--- springboot/springboot-demo/src/main/java/com/example/springbootdemo/SpringbootDemoApplication2.java
+++ springboot/springboot-demo/src/main/java/com/example/springbootdemo/SpringbootDemoApplication2.java
@@ -1,11 +1,14 @@
 package com.example.springbootdemo;
 
+import com.example.springbootdemo.configure.MyDeferredImportSelector;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Slf4j
+@Import(MyDeferredImportSelector.class)
 public class SpringbootDemoApplication2 {
 
     public static void main(String[] args) {
