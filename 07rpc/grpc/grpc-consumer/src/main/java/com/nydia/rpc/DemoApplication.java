1238e983bacd364cb2947aee7f4ebf58b185df43
nydia <nydia_lvhq@sina.cn>
2023-10-19 17:34:24 +0800

    mybatis demo

--- 07rpc/grpc/grpc-consumer/src/main/java/com/nydia/rpc/DemoApplication.java
+++ 07rpc/grpc/grpc-consumer/src/main/java/com/nydia/rpc/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.nydia.rpc;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
