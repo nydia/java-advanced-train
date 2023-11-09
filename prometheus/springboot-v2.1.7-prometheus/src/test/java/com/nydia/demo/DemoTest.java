462e10dcd6411776c326cc7360534d4c23b63cba
nydia <nydia_lvhq@sina.cn>
2023-11-09 18:48:21 +0800

    mybatis demo

---
+++ prometheus/springboot-v2.1.7-prometheus/src/test/java/com/nydia/demo/DemoTest.java
@@ -0,0 +1,19 @@
+package com.nydia.demo;
+
+/**
+ * @Description Unit Test
+ * @Date 2022/9/27 16:54
+ * @Created by <a href="mailto:nydia_lhq@hotmail.com">lvhuaqiang</a>
+ */
+public class DemoTest {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void test() {
+
+
+    }
+
+}
