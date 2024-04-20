40d85d01937550c54b8875b3e018ec9287381e1c
‘nydia’ <nydia_lhq@hotmail.com>
2024-04-20 19:37:30 +0800

    mybatis plus

--- mybatis/springboot-mybatisplus/src/main/java/com/nydia/mybatisplus/config/MyConfig.java
+++ mybatis/springboot-mybatisplus/src/main/java/com/nydia/mybatisplus/config/MyConfig.java
@@ -2,10 +2,14 @@ package com.nydia.mybatisplus.config;
 
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.DataPermissionInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import com.nydia.mybatisplus.handler.MyDataPermissionHandler;
 import com.nydia.mybatisplus.identifier.CustomIdGenerator;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -42,6 +46,20 @@ public class MyConfig {
 
         // 分页插件
         //interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.SQL_SERVER));
+
+        //多租户插件
+        interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new TenantLineHandler() {
+            @Override
+            public Expression getTenantId() {
+                return new LongValue(1);
+            }
+
+            // 这是 default 方法,默认返回 false 表示所有表都需要拼多租户条件
+            @Override
+            public boolean ignoreTable(String tableName) {
+                return !"user".equalsIgnoreCase(tableName);
+            }
+        }));
         return interceptor;
     }
 
