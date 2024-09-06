a90dc45a72d98cb6154c04f3a95b47d736109e91
‘nydia’ <nydia_lhq@hotmail.com>
2024-09-07 06:20:43 +0800

    RabbitMQ

--- 09mq/springboot-rabbitmq/src/main/java/com/nydia/rabbitmq/Constant.java
+++ 09mq/springboot-rabbitmq/src/main/java/com/nydia/rabbitmq/Constant.java
@@ -6,18 +6,24 @@ package com.nydia.rabbitmq;
  * @Description: Constant
  */
 public class Constant {
+    public static final String queue_1 = "queue-common";
+
 
     public static final String exchange_1 = "exchange-A";
-    public static final String exchange_delay = "exchange-delay";
-
+    public static final String queue_2 = "queue-A";
     public static final String routing_key_1 = "routingKey-A";
+
+
+    public static final String queue_delay = "queue-delay";
+    public static final String exchange_delay = "exchange-delay";
+    public static final String exchange_type_delay = "x-delayed-message";
     public static final String routing_key_delay = "routingKey-delay";
 
-    public static final String queue_1 = "queue-common";
-    public static final String queue_2 = "queue-A";
-    public static final String queue_delay = "queue-delay";
 
-    public static final String exchange_type_delay = "x-delayed-message";
+
+    public static final String exchange_transaction = "exchange-transaction";
+    public static final String routing_key_transaction = "routingKey-transaction";
+    public static final String queue_transaction = "queue-transaction";
 
 
 }
