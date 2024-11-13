43ddf3fdd8eecd3eb42994c43e1e141d302d78f5
‘nydia’ <nydia_lhq@hotmail.com>
2024-11-13 23:18:42 +0800

    rabbitmq接受mysql的日志同步

---
+++ 04fx/springboot-canal/springboot-canal-rabbitmq/src/main/java/com/nydia/canal/SpringbootCanalRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.nydia.canal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCanalRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCanalRabbitmqApplication.class, args);
+	}
+
+}
---
+++ 04fx/springboot-canal/springboot-canal-rabbitmq/src/test/java/com/nydia/canal/SpringbootCanalRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nydia.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCanalRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
