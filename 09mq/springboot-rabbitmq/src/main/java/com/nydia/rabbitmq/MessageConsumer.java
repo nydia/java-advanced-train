1a8cb8e8df96e63f12e6786a1a6dc54e710d7aef
‘nydia’ <nydia_lhq@hotmail.com>
2024-11-26 23:56:27 +0800

    rabbitmq

--- 09mq/springboot-rabbitmq/src/main/java/com/nydia/rabbitmq/MessageConsumer.java
+++ 09mq/springboot-rabbitmq/src/main/java/com/nydia/rabbitmq/MessageConsumer.java
@@ -1,9 +1,6 @@
 package com.nydia.rabbitmq;
 
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
